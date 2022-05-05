fd9dda5df613f7ed106b0c7f083ee8133aa68e01
Marcelo Buongermino <buon.marcelo@hotmail.com>
2022-05-05 14:10:59 -0300

    features middlewares completed

--- src/index.js
+++ src/index.js
@@ -10,19 +10,62 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+
+  const foundUser = users.find(user => user.username === username)
+
+  if(!foundUser) {
+    return response.status(404).json({ error: "User not found" })
+  }
+
+  request.user = foundUser
+  next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request
+
+  if (!user.pro && user.todos.length >= 10) {
+    return response.status(403)
+  }
+
+  next()
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const { id } = request.params
+  
+  if(!validate(id)){
+    return response.status(400)
+  }
+
+  const user = users.find(user => user.username === username)
+  if (!user) {
+    return response.status(404).json({ error: "User not found" })
+  }
+
+  const todo = user.todos.find(todo => todo.id === id)
+  if (!todo) {
+    return response.status(404).json({ error: "Task not found" })
+  }
+  request.user = user
+  request.todo = todo
+  next()
+
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params
+
+  const foundUser = users.find(user => user.id === id)
+
+  if(!foundUser) {
+    return response.status(404).json({ error: "User not found" })
+  }
+
+  request.user = foundUser
+  next()
 }
 
 app.post('/users', (request, response) => {
